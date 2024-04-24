21bae8148ac228d2d197604e674f81826e98e864
Soporte 7 Auxiliar Sena <soporte7@uros.local>
2024-04-24 18:03:03 -0500

    Avance

--- src/componentes/Computadores/Computadores.jsx
+++ src/componentes/Computadores/Computadores.jsx
@@ -5,11 +5,13 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import { Navigate } from 'react-router-dom';
+import Table from 'react-bootstrap/Table';
 
 
 export default function Computadores() {
 
   const { user } = useContext(userContext)
+  
 
   const [formData, setFormData] = useState({
     fecha: '',
@@ -34,6 +36,144 @@ export default function Computadores() {
     tecnico: '',
     observaciones: '',
   });
+
+  const [computadoress, setComputadores] = useState([]);
+  const [serial, setSerial] = useState('');
+  const [ip, setIp] = useState('');
+  const handleCloseID = () => setShowID(false);
+  const handleShowID = () => setShowID(true);
+  const [showID, setShowID] = useState(false);
+
+  const [showEliminar, setShowEliminar] = useState(false);
+  const [idEliminar, setIdEliminar] = useState('');
+  const handleCloseEli = () => setShowEliminar(false);
+  const handleShowEli = () => setShowEliminar(true);
+  
+
+  const handleDeleteComputador = (id) => {
+    setShowEliminar(true);
+    setIdEliminar(id)
+  }
+  
+
+  
+  const handleEliminarClick = async (id) => {
+
+    const computer = computadoress.find(computer => computer._id === id);
+    const numeroSerie = computer.serial;
+    console.log(computer)
+
+    try {
+
+      // Enviar la solicitud DELETE a la API sisa
+      const response = await fetch(`http://localhost:8000/api/inventario/eliminarComputadores/${id}`, {
+        method: 'DELETE',
+      });
+
+      // Manejar la respuesta
+      if (response.status === 200) {
+        // Eliminación exitosa
+        alert(`Computadores "${numeroSerie}" eliminada con éxito.`);
+      } else {
+        // Error al eliminar
+        const data = await response.json();
+        const mensajeError = data.msg || 'Error al eliminar la impresora.';
+        alert(mensajeError);
+      }
+    } catch (error) {
+      // Error inesperado
+      console.error('Error al eliminar la impresora:', error.message);
+      alert('Ha ocurrido un error inesperado. Inténtalo de nuevo más tarde.');
+    }
+  };
+
+  const obtenerComputadores = async (tipoBusqueda, valorBusqueda) => {
+    try {
+      let url;
+      if (tipoBusqueda === 'serial') {
+        url = `http://localhost:8000/api/inventario/computador/listarID/${valorBusqueda}`;
+      } else {
+        console.error('Tipo de búsqueda inválido:', tipoBusqueda);
+        return; // Manejar el tipo de búsqueda inválido
+      }
+
+      const respuesta = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const datos = await respuesta.json();
+
+      if (datos && datos.verificarPro) {
+        setComputadores([datos.verificarPro]); // Suponiendo un único resultado
+      } else {
+        console.error('API no responde o registro no encontrado.');
+        setComputadores([]); // Limpiar datos si no hay resultados
+      }
+    } catch (error) {
+      console.error('Error al obtener impresoras:', error);
+    }
+  };
+  
+  const obtenerComputadoresIP = async (tipoBusqueda, valorBusqueda) => {
+    try {
+      let url;
+      if (tipoBusqueda === 'ip') {
+        url = `http://localhost:8000/api/inventario/computador/listarIP/${valorBusqueda}`;
+      } else {
+        console.error('Tipo de búsqueda inválido:', tipoBusqueda);
+        return; // Manejar el tipo de búsqueda inválido
+      }
+
+      const respuesta = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const datos = await respuesta.json();
+
+      if (datos && datos.verificarPro) {
+        setComputadores([datos.verificarPro]); // Suponiendo un único resultado
+      } else {
+        console.error('API no responde o registro no encontrado.');
+        setComputadores([]); // Limpiar datos si no hay resultados
+      }
+    } catch (error) {
+      console.error('Error al obtener impresoras:', error);
+    }
+  };
+  useEffect(() => {
+    const fetchImpresoras = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/api/inventario/listarcompu', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        const data = await response.json();
+
+        // Ensure data has the expected structure and property
+        if (data && data.listarCompu) {
+          setComputadores(data.listarCompu);
+        } else {
+          console.error('la api no responde.');
+          // Handle the case where the API data is missing or has an unexpected structure
+        }
+      } catch (error) {
+        console.error('Error fetching impresoras:', error);
+      }
+    };
+
+    fetchImpresoras();
+  }, []);
+
+
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -95,7 +235,46 @@ export default function Computadores() {
       <Button variant="primary" onClick={handleShow}>
         Aqui agregas el computador
       </Button>
+      <Button variant="success" onClick={handleShowID}>
+        Listar por SERIAL
+      </Button>
 
+      <Modal show={showID} onHide={handleCloseID}>
+        <Modal.Header closeButton>
+          <Modal.Title>Buscar por SERIAL</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Ingrese el serial</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="serial"
+                value={serial}
+                onChange={(e) => setSerial(e.target.value) } />
+              <Button variant="success" onClick={() => obtenerComputadores('serial', serial)}>Buscar</Button>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Header closeButton>
+          <Modal.Title>Buscar por IP</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label><th>Ingrese la ip</th></Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="ip"
+                value={ip}
+                onChange={(e) => setIp(e.target.value) } />
+              <Button variant="success" onClick={() => obtenerComputadoresIP('ip', ip)}>Buscar</Button>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+        </Modal.Footer>
+      </Modal>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Quieres ingresar un computador?</Modal.Title>
@@ -320,6 +499,80 @@ export default function Computadores() {
           </Button>
         </Modal.Footer>
       </Modal>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>fecha</th>
+            <th>sede</th>
+            <th>ubicacion</th>
+            <th>nombre equipo</th>
+            <th>sistema operativo</th>
+            <th>placa</th>
+            <th>disco duro</th>
+            <th>memoria ram</th>
+            <th>ip</th>
+            <th>serial</th>
+            <th>mac</th>
+            <th>marca</th>
+            <th>usuario</th>
+            <th>clave</th>
+            <th>nombre asignado</th>
+            <th>cedula</th>
+            <th>fecha mantenimiento</th>
+            <th>tecnico</th>
+            <th>dominio</th>
+            <th>observacion</th>
+          </tr>
+        </thead>
+        <tbody>
+          {!computadoress.length && <p>Loading impresoras...</p>} {/* Conditional rendering while data is loading */}
+          {computadoress.map((regis, index) => (
+            <tr key={index}>
+              <td>{regis.fecha}</td>
+              <td>{regis.sede}</td>
+              <td>{regis.ubicacion}</td>
+              <td>{regis.nombre_equipo}</td>
+              <td>{regis.sistema_operativo}</td>
+              <td>{regis.placa}</td>
+              <td>{regis.disco_duro}</td>
+              <td>{regis.memoria_ram}</td>
+              <td>{regis.ip}</td>
+              <td>{regis.serial}</td>
+              <td>{regis.mac}</td>
+              <td>{regis.marca}</td>
+              <td>{regis.usuario}</td>
+              <td>{regis.clave}</td>
+              <td>{regis.nombre_asignado}</td>
+              <td>{regis.cedula}</td>
+              <td>{regis.fecha_mantenimiento}</td>
+              <td>{regis.tecnico}</td>
+              <td>{regis.dominio}</td>
+              <td>{regis.observaciones}</td>
+              <Button variant="danger" onClick={() => {
+                handleDeleteComputador(regis._id)
+              }}>
+                Eliminar
+              </Button>
+            </tr>
+          ))}
+        </tbody>
+        <Modal show={showEliminar} onHide={handleCloseEli}>
+          <Modal.Header closeButton>
+            <Modal.Title>¿Quieres Eliminar computador?</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>¿Estas seguro de eliminar?</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleCloseEli}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={()=>{
+              handleEliminarClick(idEliminar)
+            }}>
+             Eliminar
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </Table>
     </>
   )
 }
--- src/componentes/Home/Home.jsx
+++ src/componentes/Home/Home.jsx
@@ -1,303 +1,90 @@
-import React, { useState, useEffect } from 'react';
-import Impresorass from '../Impresoras/Impresoras'
-import Login from '../../auth/Login/Login'
-import Narvbar from '../Narvbar/Narvbar'
+import React from 'react'
 import { io } from 'socket.io-client'
+import Narvbar from '../Narvbar/Narvbar';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import './Homes.css'; // Relative path
 import Button from 'react-bootstrap/Button';
-import Table from 'react-bootstrap/Table';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
+
 
 //import Reportes from'../Reportes-tecnicos/Reportes-compu'
 
+
 export default function Home() {
 
+
   const socket = io('http://localhost:8000');
-
-  const [computadoress, setComputadores] = useState([]);
-  const [serial, setSerial] = useState('');
-  const [ip, setIp] = useState('');
-  const handleCloseID = () => setShowID(false);
-  const handleShowID = () => setShowID(true);
-  const [showID, setShowID] = useState(false);
-
-
   socket.on('connect', () => {
     console.log('conectado')
 
   })
-  const [showEliminar, setShowEliminar] = useState(false);
-
-  /*const [SeleccionarModificar, setModificar] = useState({
-    fecha: '',
-    sede: '',
-    ubicacion: '',
-    area: '',
-    marca: '',
-    nombre_equipo: '',
-    sistema_operativo: '',
-    placa: '',
-    disco_duro: '',
-    memoria_ram: '',
-    serial: '',
-    mac: '',
-    ip: '',
-    usuario: '',
-    clave: '',
-    nombre_asignado: '',
-    cedula: '',
-    dominio: '',
-    fecha_mantenimiento: '',
-    tecnico: '',
-    observaciones: '',
-
-  })*/
-
-  const handleClose = () => setShowEliminar(false);
-  const handleShow = () => setShowEliminar(true);
-
-  const [idEliminar, setIdEliminar] = useState('');
-
-  const handleDeleteComputador = (id) => {
-    setShowEliminar(true);
-    setIdEliminar(id)
-  }
-
-  const handleEliminarClick = async (id) => {
-
-    const computer = computadoress.find(computer => computer._id === id);
-    const numeroSerie = computer.serial;
-    console.log(computer)
-
-    try {
-
-      // Enviar la solicitud DELETE a la API sisa
-      const response = await fetch(`http://localhost:8000/api/inventario/eliminarComputadores/${id}`, {
-        method: 'DELETE',
-      });
-
-      // Manejar la respuesta
-      if (response.status === 200) {
-        // Eliminación exitosa
-        alert(`Computadores "${numeroSerie}" eliminada con éxito.`);
-      } else {
-        // Error al eliminar
-        const data = await response.json();
-        const mensajeError = data.msg || 'Error al eliminar la impresora.';
-        alert(mensajeError);
-      }
-    } catch (error) {
-      // Error inesperado
-      console.error('Error al eliminar la impresora:', error.message);
-      alert('Ha ocurrido un error inesperado. Inténtalo de nuevo más tarde.');
-    }
-  };
-  const obtenerComputadores = async (tipoBusqueda, valorBusqueda) => {
-    try {
-      let url;
-      if (tipoBusqueda === 'serial') {
-        url = `http://localhost:8000/api/inventario/computador/listarID/${valorBusqueda}`;
-      } else {
-        console.error('Tipo de búsqueda inválido:', tipoBusqueda);
-        return; // Manejar el tipo de búsqueda inválido
-      }
-
-      const respuesta = await fetch(url, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      const datos = await respuesta.json();
-
-      if (datos && datos.verificarPro) {
-        setComputadores([datos.verificarPro]); // Suponiendo un único resultado
-      } else {
-        console.error('API no responde o registro no encontrado.');
-        setComputadores([]); // Limpiar datos si no hay resultados
-      }
-    } catch (error) {
-      console.error('Error al obtener impresoras:', error);
-    }
-  };
-  const obtenerComputadoresIP = async (tipoBusqueda, valorBusqueda) => {
-    try {
-      let url;
-      if (tipoBusqueda === 'ip') {
-        url = `http://localhost:8000/api/inventario/computador/listarIP/${valorBusqueda}`;
-      } else {
-        console.error('Tipo de búsqueda inválido:', tipoBusqueda);
-        return; // Manejar el tipo de búsqueda inválido
-      }
-
-      const respuesta = await fetch(url, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      const datos = await respuesta.json();
-
-      if (datos && datos.verificarPro) {
-        setComputadores([datos.verificarPro]); // Suponiendo un único resultado
-      } else {
-        console.error('API no responde o registro no encontrado.');
-        setComputadores([]); // Limpiar datos si no hay resultados
-      }
-    } catch (error) {
-      console.error('Error al obtener impresoras:', error);
-    }
-  };
-
-  useEffect(() => {
-    const fetchImpresoras = async () => {
-      try {
-        const response = await fetch('http://localhost:8000/api/inventario/listarcompu', {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
-
-        const data = await response.json();
-
-        // Ensure data has the expected structure and property
-        if (data && data.listarCompu) {
-          setComputadores(data.listarCompu);
-        } else {
-          console.error('la api no responde.');
-          // Handle the case where the API data is missing or has an unexpected structure
-        }
-      } catch (error) {
-        console.error('Error fetching impresoras:', error);
-      }
-    };
-
-    fetchImpresoras();
-  }, []);
-
-
-
   return (
     <>
-     <Narvbar />
-      <Button variant="success" onClick={handleShowID}>
-        Listar por SERIAL
-      </Button>
 
-      <Modal show={showID} onHide={handleCloseID}>
-        <Modal.Header closeButton>
-          <Modal.Title>Buscar por SERIAL</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Ingrese el serial</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="serial"
-                value={serial}
-                onChange={(e) => setSerial(e.target.value) } />
-              <Button variant="success" onClick={() => obtenerComputadores('serial', serial)}>Buscar</Button>
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Header closeButton>
-          <Modal.Title>Buscar por IP</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label><th>Ingrese la ip</th></Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="ip"
-                value={ip}
-                onChange={(e) => setIp(e.target.value) } />
-              <Button variant="success" onClick={() => obtenerComputadoresIP('ip', ip)}>Buscar</Button>
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-        </Modal.Footer>
-      </Modal>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>fecha</th>
-            <th>sede</th>
-            <th>ubicacion</th>
-            <th>nombre equipo</th>
-            <th>sistema operativo</th>
-            <th>placa</th>
-            <th>disco duro</th>
-            <th>memoria ram</th>
-            <th>ip</th>
-            <th>serial</th>
-            <th>mac</th>
-            <th>marca</th>
-            <th>usuario</th>
-            <th>clave</th>
-            <th>nombre asignado</th>
-            <th>cedula</th>
-            <th>fecha mantenimiento</th>
-            <th>tecnico</th>
-            <th>dominio</th>
-            <th>observacion</th>
-          </tr>
-        </thead>
-        <tbody>
-          {!computadoress.length && <p>Loading impresoras...</p>} {/* Conditional rendering while data is loading */}
-          {computadoress.map((regis, index) => (
-            <tr key={index}>
-              <td>{regis.fecha}</td>
-              <td>{regis.sede}</td>
-              <td>{regis.ubicacion}</td>
-              <td>{regis.nombre_equipo}</td>
-              <td>{regis.sistema_operativo}</td>
-              <td>{regis.placa}</td>
-              <td>{regis.disco_duro}</td>
-              <td>{regis.memoria_ram}</td>
-              <td>{regis.ip}</td>
-              <td>{regis.serial}</td>
-              <td>{regis.mac}</td>
-              <td>{regis.marca}</td>
-              <td>{regis.usuario}</td>
-              <td>{regis.clave}</td>
-              <td>{regis.nombre_asignado}</td>
-              <td>{regis.cedula}</td>
-              <td>{regis.fecha_mantenimiento}</td>
-              <td>{regis.tecnico}</td>
-              <td>{regis.dominio}</td>
-              <td>{regis.observaciones}</td>
-              <Button variant="danger" onClick={() => {
-                handleDeleteComputador(regis._id)
-              }}>
-                Eliminar
-              </Button>
-            </tr>
-          ))}
-        </tbody>
-        <Modal show={showEliminar} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>¿Quieres Eliminar computador?</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>¿Estas seguro de eliminar?</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={()=>{
-              handleEliminarClick(idEliminar)
-            }}>
-             Eliminar
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </Table>
 
+
+      <Narvbar />
+      <body>
+        <div style={{ backgroundColor: 'red', display: "flex", justifyContent: "center" }}>
+          <Card style={{ width: '300px', justifyItems: 'center', display: 'block', textAlign: 'center' }}>
+            <Card variant="top" style={{ display: 'block', justifyItems: 'center' }} />
+            <h1 className='bi bi-pc-display-horizontal' ></h1>
+            <Card.Body>
+              <Card.Title>Modulo de computadores</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>Cras justo odio</ListGroup.Item>
+              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Button>Ingrese al modulo</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '300px', justifyItems: 'center', display: 'block', textAlign: 'center' }}>
+            <Card variant="top" style={{ display: 'block', justifyItems: 'center' }}/>
+            <h1 className="bi bi-printer"></h1>
+            <Card.Body>
+              <Card.Title>Modulo de impresoras</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>Cras justo odio</ListGroup.Item>
+              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Button>Ingrese al modulo</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '300px', justifyItems: 'center', display: 'block', textAlign: 'center' }}>
+            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Body>
+              <Card.Title>Modulos Reportes</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>Cras justo odio</ListGroup.Item>
+              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Button>Ingrese al modulo</Button>
+            </Card.Body>
+          </Card>
+        </div>
+      </body>
     </>
   )
+
 }
